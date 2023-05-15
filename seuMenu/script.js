55e29572da636cc87398398c7fc9be1ddeffedb9
Isabella Cavalcante <108815118+IsabellaMyreah@users.noreply.github.com>
2023-05-15 12:42:27 -0300

    Add files via upload

--- seuMenu/script.js
+++ seuMenu/script.js
@@ -3,7 +3,7 @@ const menu = [
         id: 1,
         title: "Buttermilk Pancakes",
         category: "Breakfast",
-        price: 15.99,
+        price: "R$ 15.99",
         image: "./images/item-1.jpeg",
         description: "I'm baby woke milkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed.",
     },
@@ -12,7 +12,7 @@ const menu = [
         id: 2,
         title: "Diner Double",
         category: "Lunch",
-        price: 13.99,
+        price: "R$ 13.99",
         image: "./images/item-2.jpeg",
         description: "Vaporware iphone mumnlecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing.",
     },
@@ -21,7 +21,7 @@ const menu = [
         id: 3,
         title: "Godzilla Milkshake",
         category: "Shakes",
-        price: 6.99,
+        price: "R$ 6.99",
         image: "./images/item-3.jpeg",
         description: "ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral",
     },
@@ -30,7 +30,7 @@ const menu = [
         id: 4,
         title: "Country Delight",
         category: "Breakfast",
-        price: 20.99,
+        price: "R$ 20.99",
         image: "./images/item-4.jpeg",
         description: "Shabby chic keffiyeh neutra snackwave pork belly shoreditch.Prism austin mlkshk truffaut.",
     },
@@ -39,7 +39,7 @@ const menu = [
         id: 5,
         title: "Egg Attack",
         category: "Lunch",
-        price: 22.99,
+        price: "R$ 22.99",
         image: "./images/item-5.jpeg",
         description: "Franzen vegan pabst bicycle rights kickstater pinterest meditation farm-to-table 90's pop-up",
     },
@@ -48,7 +48,7 @@ const menu = [
         id: 6,
         title: "Oreo Dream",
         category: "Shakes",
-        price: 18.99,
+        price: "R$ 18.99",
         image: "./images/item-6.jpeg",
         description: "Portland chicharrones ethical edison, palo santo craft beer chia heirloom iphone everyday",
     },
@@ -57,7 +57,7 @@ const menu = [
         id: 7,
         title: "Bacon Overflow",
         category: "Breakfast",
-        price: 8.99,
+        price: "R$ 8.99",
         image: "./images/item-7.jpeg",
         description: "Carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird",
     },
@@ -66,7 +66,7 @@ const menu = [
         id: 8,
         title: "American Classic",
         category: "Lunch",
-        price: 12.99,
+        price: "R$ 12.99",
         image: "./images/item-8.jpeg",
         description: "On it tumblr kickstarter thundercast migas everyday carry squid palo santo leggings. Food truck truffaut",
     },
@@ -75,7 +75,7 @@ const menu = [
         id: 9,
         title: "Quarantine Buddy",
         category: "Shakes",
-        price: 16.99,
+        price: "R$ 16.99",
         image: "./images/item-9.jpeg",
         description: "Skateboard fam synth authentic semiotics. Live-edge lyft af, edison buld yuccie crucifix microdosing",
     },
@@ -84,30 +84,33 @@ const menu = [
         id: 10,
         title: "Stack Dinner",
         category: "Dinner",
-        price: 39.99,
+        price: "R$ 39.99",
         image: "./images/item-10.jpeg",
         description: "Skateboard fam synth authentic semiotics. Live-edge lyft af, edison buld yuccie crucifix microdosing",
     }
 ];
 
 const menuItems = document.querySelector('.menu-items');
-const buttonContainer = document.querySelector('.button-container');
+const buttonContainer = document.querySelector('.button-container'); 
 
 window.addEventListener("DOMContentLoaded", () => {
     displayMenuButtons()
 })
 
-menu.map((item) => {
-    let html = `
+function displayMenuItems(menuItem) {
+let showMenu = menuItem.map((item) => {
+    return `
     <div class="content">
     <div class="item-image">
         <img src="${item.image}" alt="${item.title}" />
     </div>
+
     <div class="item-info">
         <div class="item-title">
         <p>${item.title}</p>
         <p class="price">${item.price}</p>
     </div>
+
     <div class="item-description">
         <p>
          ${item.description}
@@ -117,9 +120,12 @@ menu.map((item) => {
     </div>
     `
 
-    menuItems.innerHTML += html
     });
-    
+
+    showMenu = showMenu.join("");
+    menuItems.innerHTML = showMenu;
+    }
+
     function displayMenuButtons() {
         const categories = menu.reduce((value, item) => {
             if(!value.includes(item.category)){
@@ -137,4 +143,25 @@ menu.map((item) => {
         }).join("");
 
         buttonContainer.innerHTML += categoryButtons;
+
+        const filterButtons = document.querySelectorAll(".filter-button");
+
+        filterButtons.forEach((button) => {
+            button.addEventListener("click", (event) => {
+                const category = event.currentTarget.dataset.id;
+
+                const menuCategory = menu.filter((menuItem) => {
+                    if(menuItem.category === category){
+                        return menuItem;
+                    }
+                });
+
+                if(category === "all") {
+                    displayMenuItems(menu);
+                } else {
+                    displayMenuItems(menuCategory);
+                }
+            });
+        });
     }
+
